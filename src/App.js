30db4c662e69ae114afd42fec7b2512528aebba0
Gwenaël Magnenat <g.magnenat@gmail.com>
2022-01-21 16:55:09 +0100

    Add WebcampCapture component and import react-webcam. Basic webcam settings.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import './App.css';
+import WebcamCapture from './components/WebcamCapture';
 
 function App() {
 	return (
 		<div className="App">
 			<h1>Let's build snapchat</h1>
+			<WebcamCapture />
 		</div>
 	);
 }
