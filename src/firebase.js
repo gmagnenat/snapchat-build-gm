6e0a6be554b2ac1e28f02dc7c127f21c5665961b
Gwenaël Magnenat <g.magnenat@gmail.com>
2022-01-21 16:46:03 +0100

    Firebase setup

--- src/firebase.js
+++ src/firebase.js
@@ -1,3 +1,27 @@
+import { initializeApp } from 'firebase/app';
+
+import {
+	getFirestore,
+	collection,
+	getDocs,
+	addDoc,
+	deleteDoc,
+	serverTimestamp,
+	doc,
+	orderBy,
+	limit,
+	onSnapshot,
+	query,
+} from 'firebase/firestore';
+
+import {
+	getAuth,
+	GoogleAuthProvider,
+	signInWithPopup,
+	signOut,
+	onAuthStateChanged,
+} from 'firebase/auth';
+
 const firebaseConfig = {
 	apiKey: 'AIzaSyADNNYKc11zRxcxTupX1FhxBSUG0qVrGrU',
 	authDomain: 'snapshat-clone-gm.firebaseapp.com',
@@ -5,4 +29,29 @@ const firebaseConfig = {
 	storageBucket: 'snapshat-clone-gm.appspot.com',
 	messagingSenderId: '327727920784',
 	appId: '1:327727920784:web:8dcf20fbf1d8c69bda0a15',
+};
+
+initializeApp(firebaseConfig);
+
+const db = getFirestore();
+const auth = getAuth();
+const googleProvider = new GoogleAuthProvider();
+
+export {
+	db,
+	collection,
+	getDocs,
+	addDoc,
+	deleteDoc,
+	serverTimestamp,
+	doc,
+	auth,
+	orderBy,
+	limit,
+	onSnapshot,
+	query,
+	googleProvider,
+	signInWithPopup,
+	signOut,
+	onAuthStateChanged,
 };
