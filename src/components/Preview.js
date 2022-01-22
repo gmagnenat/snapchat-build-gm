3ce40bc9a460c2ecac0e44dec7f3f712b843875d
Gwenaël Magnenat <g.magnenat@gmail.com>
2022-01-22 14:41:12 +0100

    Add a close button and navigate back to first screen

--- src/components/Preview.js
+++ src/components/Preview.js
@@ -1,15 +1,28 @@
-import React from 'react';
-import { useSelector } from 'react-redux';
-import { selectCameraImage } from '../features/cameraSlice';
+import React, { useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { selectCameraImage, resetCameraImage } from '../features/cameraSlice';
+import { useNavigate } from 'react-router-dom';
+import CloseIcon from '@mui/icons-material/Close';
 import './Preview.css';
 
 function Preview() {
 	const cameraImage = useSelector(selectCameraImage);
+	const navigate = useNavigate();
+	const dispatch = useDispatch();
 
-	console.log(cameraImage);
+	useEffect(() => {
+		if (!cameraImage) {
+			navigate('/');
+		}
+	}, [cameraImage, navigate]);
+
+	const closePreview = () => {
+		dispatch(resetCameraImage());
+	};
+
 	return (
 		<div className="preview">
-			<h2>This is your preview</h2>
+			<CloseIcon onClick={closePreview} className="preview__close" />
 			<img src={cameraImage} alt="" />
 		</div>
 	);
