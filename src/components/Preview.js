8a692c3416a36679d696ecf143c699b0ff897f6e
Gwenaël Magnenat <g.magnenat@gmail.com>
2022-01-22 14:49:19 +0100

    Add toolbar icons on the preview screen

--- src/components/Preview.js
+++ src/components/Preview.js
@@ -3,6 +3,13 @@ import { useSelector, useDispatch } from 'react-redux';
 import { selectCameraImage, resetCameraImage } from '../features/cameraSlice';
 import { useNavigate } from 'react-router-dom';
 import CloseIcon from '@mui/icons-material/Close';
+import TextFieldsIcon from '@mui/icons-material/TextFields';
+import CreateIcon from '@mui/icons-material/Create';
+import NoteIcon from '@mui/icons-material/Note';
+import MusicNoteIcon from '@mui/icons-material/MusicNote';
+import AttachFileIcon from '@mui/icons-material/AttachFile';
+import CropIcon from '@mui/icons-material/Crop';
+import TimerIcon from '@mui/icons-material/Timer';
 import './Preview.css';
 
 function Preview() {
@@ -23,6 +30,16 @@ function Preview() {
 	return (
 		<div className="preview">
 			<CloseIcon onClick={closePreview} className="preview__close" />
+			<div className="preview__toolbarRight">
+				<TextFieldsIcon />
+				<CreateIcon />
+				<NoteIcon />
+				<MusicNoteIcon />
+				<AttachFileIcon />
+				<CropIcon />
+				<TimerIcon />
+			</div>
+
 			<img src={cameraImage} alt="" />
 		</div>
 	);
